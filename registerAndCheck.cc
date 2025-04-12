6e26b4b123b9928922d344b3e95e9e7f8bcdaeb2
unknown <hrithik+github@sarkar.gg>
2025-04-12 13:28:19 -0400

    Added: Sign up: checks if username is taken, checks if file where users are stored exists, writes new users to file, and doesn't allow duplicate account creation.

---
+++ registerAndCheck.cc
@@ -0,0 +1,51 @@
+#include "sight.h"
+using json = nlohmann::json;
+
+//checks if users.json exists
+static bool fileExists(const std::string& users) {
+	struct stat buf;
+
+	if (stat(users.c_str(), &buf) != -1) { return true; }
+
+	return false;
+}
+
+//checks if username is already taken
+bool infoCheckSignUp(const std::string& userData) {
+	if (!fileExists("users.json")) {
+		return false;
+	}
+	std::ifstream users_file("users.json");
+	json usersData = json::parse(users_file);
+	users_file.close();
+
+	for (const json& item : usersData["users"]) {
+		if (item.contains("username") and item["username"] == userData) {
+			return true;
+		}
+	}
+	return false; 
+}
+
+//if username isn't taken then allows user to register an account
+bool registerUser(const std::string& username, const std::string& password) {
+
+	json newUser;
+
+	if (!fileExists("users.json")) {
+		return false;
+	}
+	std::ifstream users_file("users.json");
+	json usersData = json::parse(users_file);
+	users_file.close();
+
+	newUser["username"] = username;
+	newUser["password"] = password;
+
+	usersData["users"].push_back(newUser);
+	std::ofstream users_file_out("users.json");
+	users_file_out << usersData.dump(4);
+	users_file_out.close();
+
+	return true;
+}
