98f011ccc2e34262cf46e6165febbf3d1c87e6af
unknown <hrithik+github@sarkar.gg>
2025-04-13 18:21:14 -0400

    Add: Cookie string recieve and store.

--- loginCheck.cc
+++ loginCheck.cc
@@ -3,6 +3,7 @@ using json = nlohmann::json;
 
 //map to store and split user name pass from html req, could also use streamview for this btw
 std::unordered_map<std::string, std::string> parseFormData(const std::string& body) {
+
     std::unordered_map<std::string, std::string> userPass;
     std::stringstream ss(body);
     std::string pair;
@@ -18,6 +19,28 @@ std::unordered_map<std::string, std::string> parseFormData(const std::string& bo
 
     return userPass;
 }
+
+//hashtable that's gonna store username from cookies and session id (also cookie spliter)
+std::unordered_map<std::string, std::string> parseCookies(const std::string& cookieHeader) {
+
+    std::unordered_map<std::string, std::string> cookies;
+    std::stringstream ss(cookieHeader);
+    std::string pair;
+
+    while (std::getline(ss, pair, ';')) {
+        std::stringstream pairStream(pair);
+        std::string cookie, sessionID;
+        std::getline(pairStream, cookie, '=');
+        std::getline(pairStream, sessionID);
+
+        // get rid of space
+        cookie.erase(0, cookie.find_first_not_of(" "));
+
+        cookies[cookie] = sessionID;
+    }
+
+    return cookies;
+};
 
 //checks if login info is correct
 bool infoCheckLogin(const std::string & username, const std::string & password) {
